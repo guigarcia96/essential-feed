5e39f382832c8fcbbc2127bacb494b81664c4320
Guilherme Garcia <gmoraes1@outlook.com>
2024-08-10 18:49:09 -0300

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Guilherme Garcia on 10/08/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
