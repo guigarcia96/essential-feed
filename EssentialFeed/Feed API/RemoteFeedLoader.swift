18ac009bbd5fba3b44659afd8d1288116d83e881
Guilherme Garcia <guilherme.garcia@ifood.com.br>
2024-10-06 12:43:32 -0300

    move HttpClient to it own file

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -1,20 +1,4 @@
-//
-//  RemoteFeedLoader.swift
-//  EssentialFeed
-//
-//  Created by Guilherme Garcia on 11/08/24.
-//
-
 import Foundation
-
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
 
 public final class RemoteFeedLoader {
     
